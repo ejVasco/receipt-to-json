3b6182a64f2f15d694ced5dd28d7abfe6d072ff9
ejVasco <111936685+ejVasco@users.noreply.github.com>
2026-04-28 13:39:21 -0700

    new config file for paths

---
+++ config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+# All PATHS are from project root dir
+
+
+# these are for training
+LABELS_DIR = Path("data/labels")
+OCR_DIR = Path("data/ocr")
+PNG_DIR = Path("data/png")
+PDF_DIR = Path("data/raw_pdf")
